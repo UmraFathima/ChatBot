97e90d8280d62a44efce4ba9d942ca7befdcc19f
Umra Fathima <154687173+UmraFathima@users.noreply.github.com>
2025-07-06 13:21:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import os
@@ -88,3 +89,5 @@ def chat():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
